4eccc8ad5eeede396e431278da3d7a97d9165e04
shnoogie <purplefanblade@gmail.com>
2022-09-20 15:14:49 -0700

    Update syncfiles.sh
    added targeted sync

--- syncfiles.sh
+++ syncfiles.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 SOURCE_DIRECTORY=/var/usb/data/
-MOUNT_PATH=$1
-PID_FILE=/var/run/syncfiles.pid
+DEVICE_UUID=$1
+PID_FILENAME="syncfiles"
+PID_PATH=/var/run/
+PID_FILE="${PID_PATH}${PID_FILENAME}.pid"
 
 if test -f "$PID_FILE"
 then
@@ -11,16 +13,34 @@ then
 fi
 
 full_sync () {
-  echo "full sync"
+	# full sync stops all other syncs and runs a sync on every connected drive
+	# check to see if a drive was connected at the end
+	echo "full sync"
 }
 
 targeted_sync () {
-  rsync -ac --delete "${SOURCE_DIRECTORY}" $MOUNT_PATH
+	UUID_PID_FILE="${PID_FILENAME}_${DEVICE_UUID}.pid"
+
+	if test -f "$UUID_PID_FILE"
+	then
+		echo "Another sync process running for ${DEVICE_UUID}, exiting."
+    	exit 1
+    elif test -f "$PID_FILE"
+    then
+    	echo "Another sync process running, exiting."
+    	exit 1
+	fi
+
+	MOUNT_PATH="/media/${DEVICE_UUID}"
+	echo "$$" > "${PID_PATH}syncfiles_${DEVICE_UUID}.pid"
+	rsync -ac --delete "${SOURCE_DIRECTORY}" "${MOUNT_PATH}"
+
+	rm "${PID_PATH}${PID_FILENAME}_${DEVICE_UUID}.pid"
 }
 
 main () {
 
-	if [ -z ${MOUNT_PATH} ]
+	if [ -z ${DEVICE_UUID} ]
 	then
 	    full_sync
 	else
