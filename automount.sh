e9e699a333656cecde56b2bd6990780a84f21db3
shnoogie <purplefanblade@gmail.com>
2022-09-20 12:47:30 -0700

    Update automount.sh

--- automount.sh
+++ automount.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-# Add ignore for EFI and Small Mount Points
-
 PART=$1
 FS_LABEL=`lsblk -o name,label | grep ${PART} | awk '{print $2}'`
+FS_TYPE=`lsblk -o name,parttypename | grep ${PART} | awk '{print $2}'`
 UUID=`lsblk -o name,uuid | grep ${PART} | awk '{print $2}'`
+SEARCH_RE="EFI"
+
+if [[ $FS_TYPE =~ $SEARCH_RE ]]
+then
+  echo "Ignoring EFI filesystem for /dev/${PART}" | tee /dev/kmsg
+  exit 1
+fi
 
 mkdir /media/${UUID}
 /usr/bin/mount /dev/${PART} /media/${UUID} -o umask=000,noatime,sync
