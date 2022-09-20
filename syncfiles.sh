fea1f12cb5298704e670953b0fb9467a91088ae8
shnoogie <purplefanblade@gmail.com>
2022-09-20 16:58:03 -0700

    Update syncfiles.sh

--- syncfiles.sh
+++ syncfiles.sh
@@ -5,6 +5,7 @@ DEVICE_UUID=$1
 PID_FILENAME="syncfiles"
 PID_PATH=/var/run/
 PID_FILE="${PID_PATH}${PID_FILENAME}.pid"
+DEVICE_RE="/media/*"
 
 if test -f "$PID_FILE"
 then
@@ -13,13 +14,31 @@ then
 fi
 
 full_sync () {
-	# full sync stops all other syncs and runs a sync on every connected drive
-	# check to see if a drive was connected at the end
-	echo "full sync"
+	echo "$$" > "$PID_FILE"
+
+	for FILE in ${PID_PATH}${PID_RE}
+	do
+		if [ "${PID_PATH}${PID_RE}" != "$FILE" ]
+		then
+			RUNNING_PID=`cat "$FILE"`
+			kill -n 9 $RUNNING_PID
+			rm -f "$FILE"
+		fi
+	done
+
+	for DEVICE in $DEVICE_RE; do
+		if [ "$DEVICE_RE" != "$DEVICE" ]
+		then
+			rsync -ac --delete "${SOURCE_DIRECTORY}" "${DEVICE}"
+		fi
+	done
+
+	rm "$PID_FILE"
 }
 
 targeted_sync () {
-	UUID_PID_FILE="${PID_FILENAME}_${DEVICE_UUID}.pid"
+	UUID_PID_FILE="${PID_PATH}${PID_FILENAME}_${DEVICE_UUID}.pid"
+	MOUNT_PATH="/media/${DEVICE_UUID}"
 
 	if test -f "$UUID_PID_FILE"
 	then
@@ -31,15 +50,14 @@ targeted_sync () {
     	exit 1
 	fi
 
-	MOUNT_PATH="/media/${DEVICE_UUID}"
-	echo "$$" > "${PID_PATH}syncfiles_${DEVICE_UUID}.pid"
+	echo "$$" > "$UUID_PID_FILE"
+
 	rsync -ac --delete "${SOURCE_DIRECTORY}" "${MOUNT_PATH}"
 
 	rm "${PID_PATH}${PID_FILENAME}_${DEVICE_UUID}.pid"
 }
 
 main () {
-
 	if [ -z ${DEVICE_UUID} ]
 	then
 	    full_sync
@@ -49,5 +67,3 @@ main () {
 }
 
 main
-
-#rm /var/run/syncfiles.pid
