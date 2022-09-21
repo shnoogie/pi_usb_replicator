0f10c3e8e95d1812e137d311c8b20003df8fa0f9
shnoogie <purplefanblade@gmail.com>
2022-09-20 21:06:56 -0700

    Update folderwatch.sh

--- folderwatch.sh
+++ folderwatch.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-while inotifywait -qq-r --exclude '.*\.part' -e moved_to -e create -e delete /var/usb/data/; do
-  /usr/local/bin/syncfiles.sh
+while inotifywait -qq -r --exclude '.*\.part' -e moved_to -e create -e delete /var/usb/data/
+do
+	echo "Changes detected syncing USBs" | tee /dev/kmsg
+	/usr/local/bin/syncfiles.sh
 done
