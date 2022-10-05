950d6b09c4be7197ffb40c50a10f50f51c2fba59
shnoogie <purplefanblade@gmail.com>
2022-10-05 09:16:50 -0700

    Update automount.sh
    when testing with bulk usbs, found lots of duplicate uuids, adding partition to folder name to prevent issues when mounting.

--- automount.sh
+++ automount.sh
@@ -4,6 +4,7 @@ PART=$1
 FS_LABEL=`lsblk -o name,label | grep ${PART} | awk '{print $2}'`
 FS_TYPE=`lsblk -o name,parttypename | grep ${PART} | awk '{print $2}'`
 UUID=`lsblk -o name,uuid | grep ${PART} | awk '{print $2}'`
+FOLDER_NAME="${PART}-${UUID}"
 SEARCH_RE="EFI"
 
 if [[ $FS_TYPE =~ $SEARCH_RE ]]
@@ -12,14 +13,14 @@ then
   exit 1
 fi
 
-mkdir /media/${UUID}
-/usr/bin/mount /dev/${PART} /media/${UUID} -o umask=000,noatime,sync
+mkdir /media/${FOLDER_NAME}
+/usr/bin/mount /dev/${PART} /media/${FOLDER_NAME} -o umask=000,noatime,sync
 
 if [ $? -eq 0 ]
 then
-  echo "Mounted /dev/$PART to /media/$UUID" | tee /dev/kmsg
-  /usr/local/bin/syncfiles.sh "$UUID"
+  echo "Mounted /dev/$PART to /media/$FOLDER_NAME" | tee /dev/kmsg
+  /usr/local/bin/syncfiles.sh "$FOLDER_NAME"
 else
-  echo "Failed to mount /dev/$PART deleting /media/$UUID" | tee /dev/kmsg
-  rm -fr /media/${UUID}
+  echo "Failed to mount /dev/$PART deleting /media/$FOLDER_NAME" | tee /dev/kmsg
+  rm -fr /media/${FOLDER_NAME}
 fi
